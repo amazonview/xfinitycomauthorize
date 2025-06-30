5ba2776d03fee8de0eeadddb7c392dd2a2f490c4
amazonview <chetijasimran@gmail.com>
2025-06-30 14:41:43 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'xfinity authorize'
 
 
 # The full version, including alpha/beta/rc tags
